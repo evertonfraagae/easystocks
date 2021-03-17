b22315e2d6c796ecfca2bcadf2a6caf1088fb1a3
Everton <evertonfraagae>
2021-03-17 14:51:22 +0100

    initial commit

---
+++ easyStocks/src/main/java/com/efraga/easystocks/EasyStocksApplication.java
@@ -0,0 +1,13 @@
+package com.efraga.easystocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyStocksApplication.class, args);
+	}
+
+}
---
+++ easyStocks/src/test/java/com/efraga/easystocks/EasyStocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efraga.easystocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyStocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
